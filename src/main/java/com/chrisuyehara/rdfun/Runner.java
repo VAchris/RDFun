222109bee49c6f7d74e6ca7e900c5281c0d9c7ce
VAchris <chris.uyehara@va.gov>
2012-11-27 18:56:50 -0800

    M bug fix and increased verbosity

--- src/main/java/com/chrisuyehara/rdfun/Runner.java
+++ src/main/java/com/chrisuyehara/rdfun/Runner.java
@@ -70,7 +70,14 @@ public class Runner {
                 MappedNode node = new MappedNode();
 
                 node.setCode(strings[0]);
+                /* don't map if no entry exists */
+                if (null == strings[7] || "" == strings[7].trim()) {
+                    continue;
+                }
                 node.setToCode(strings[7]);
+                if (null == strings[8] || "" == strings[8].trim()) {
+                    continue;
+                }
                 node.setToLabel(strings[8].replace('^', '|'));
                 node.setLabel(strings[1].replace('^', '|'));
 
